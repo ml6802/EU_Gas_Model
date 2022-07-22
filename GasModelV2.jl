8698db20ec9b266e264bef1554c09fc723e903b5
ml6802 <ml6802@princeton.edu>
2022-07-22 14:50:52 -0700

    Added new input file conversions

--- GasModelV2.jl
+++ GasModelV2.jl
@@ -812,7 +812,7 @@ function main()
     folder = "C:\\Users\\mike_\\Documents\\ZeroLab\\EU_Gas_Model"
     input = "Inputs"
     input_path = joinpath(folder, input)
-    post = "Post_New"
+    post = "Post_Accel_Hydro"
     post_path = joinpath(input_path, post)
     outputs = "Outputs"
     lngcsv = "plotting_allcases.csv"
